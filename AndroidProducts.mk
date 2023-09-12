112e626a18003e600389b20ccfd548576203e157
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-12 19:01:34 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gsi_arm64_vN.mk \
     $(LOCAL_DIR)/lineage_gsi_arm64_vS.mk \
     $(LOCAL_DIR)/lineage_gsi_arm64_gN.mk \
-    $(LOCAL_DIR)/lineage_generic.mk
+    $(LOCAL_DIR)/lineage_gsi.mk
